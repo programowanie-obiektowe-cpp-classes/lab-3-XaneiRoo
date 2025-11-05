95218ca91157c32f85ce8f918946d2a30c0cffc5
jankr <jankr@LENOVO-LEGION-5>
2025-11-05 23:40:16 +0100

    wykonanie zejściówki

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,30 @@
 #pragma once
-
+//#include <print>
 #include "Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    //std::println("liczba towaru {}", towar);
+    unsigned int przetransportowano = 0;
+    unsigned int liczbaZaglowcow = 0;
+
+    Stocznia stocznia{};
+
+    while (przetransportowano < towar)
+    {
+        Statek* nowyStatek = stocznia();
+
+        przetransportowano += nowyStatek->transportuj();
+        //std::println("przetransportowano {}", przetransportowano);
+        if (dynamic_cast<Zaglowiec*>(nowyStatek) != nullptr)
+        {
+            liczbaZaglowcow++;
+        }
+
+        delete nowyStatek;
+    }
+    //std::println("liczba zaglowcow {}", liczbaZaglowcow);
+    return liczbaZaglowcow;
+
+    
 }
